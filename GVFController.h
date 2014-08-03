3b563795017d9ea8e7c633ea46071949c6a6542a
David Ryskalczyk <d235j.1@gmail.com>
2014-08-03 19:10:53 -0400

    Use spaces instead of tabs

--- GVFController.h
+++ GVFController.h
@@ -4,10 +4,10 @@
 //
 //  Created by David Ryskalczyk on 2014/08/23.
 //  Copyright 2014 David Ryskalczyk All rights reserved.
-//	GVFixer is released under the MIT License.
+//  GVFixer is released under the MIT License.
 //  Thanks to cvz for providing a nice template
 //  in the form of ColorfulSidebar!
-//	http://opensource.org/licenses/mit-license.php
+//  http://opensource.org/licenses/mit-license.php
 //
 
 #import <Cocoa/Cocoa.h>
--- GVFController.m
+++ GVFController.m
@@ -4,10 +4,10 @@
 //
 //  Created by David Ryskalczyk on 2014/08/23.
 //  Copyright 2014 David Ryskalczyk All rights reserved.
-//	GVFixer is released under the MIT License.
+//  GVFixer is released under the MIT License.
 //  Thanks to cvz for providing a nice template
 //  in the form of ColorfulSidebar!
-//	http://opensource.org/licenses/mit-license.php
+//  http://opensource.org/licenses/mit-license.php
 //
 
 #import "GVFController.h"
@@ -15,17 +15,17 @@
 
 
 void GVFSwizzleInstanceMethod (Class cls, SEL old, SEL new) {
-	Method mold = class_getInstanceMethod(cls, old);
-	Method mnew = class_getInstanceMethod(cls, new);
-	if (mold && mnew) {
-		if (class_addMethod(cls, old, method_getImplementation(mold), method_getTypeEncoding(mold))) {
-			mold = class_getInstanceMethod(cls, old);
-		}
-		if (class_addMethod(cls, new, method_getImplementation(mnew), method_getTypeEncoding(mnew))) {
-			mnew = class_getInstanceMethod(cls, new);
-		}
-		method_exchangeImplementations(mold, mnew);
-	}
+    Method mold = class_getInstanceMethod(cls, old);
+    Method mnew = class_getInstanceMethod(cls, new);
+    if (mold && mnew) {
+        if (class_addMethod(cls, old, method_getImplementation(mold), method_getTypeEncoding(mold))) {
+            mold = class_getInstanceMethod(cls, old);
+        }
+        if (class_addMethod(cls, new, method_getImplementation(mnew), method_getTypeEncoding(mnew))) {
+            mnew = class_getInstanceMethod(cls, new);
+        }
+        method_exchangeImplementations(mold, mnew);
+    }
 }
 
 
@@ -53,7 +53,7 @@ void GVFSwizzleInstanceMethod (Class cls, SEL old, SEL new) {
     
     NSData *newData = [sdata dataUsingEncoding:NSUTF8StringEncoding];
 
-	[self GVF_accountInfoFetcher:fetcher finishedWithData:newData error:err];
+    [self GVF_accountInfoFetcher:fetcher finishedWithData:newData error:err];
 }
 
 @end
@@ -70,6 +70,6 @@ void GVFSwizzleInstanceMethod (Class cls, SEL old, SEL new) {
     old = @selector(accountInfoFetcher:finishedWithData:error:);
     new = @selector(GVF_accountInfoFetcher:finishedWithData:error:);
     GVFSwizzleInstanceMethod(cls, old, new);
-			
+            
 }
 @end
